529efdbd80af7b70390b7d9846ca087fab0be543
uShire <afgregana@up.edu.ph>
2024-11-19 08:02:40 +0800

    first commit

---
+++ src/questions.jsx
@@ -0,0 +1,111 @@
+export const questions =[
+  {
+    question_aid: 1,
+    question_question:"Q1 Maganda ba ako?",
+    choice: [
+      {
+        choice: "Pwede",
+        isCorrect: false,
+      },
+      {
+        choice: "Oo",
+        isCorrect: true,
+      },
+      {
+        choice: "Siguro",
+        isCorrect: false,
+      },
+      {
+        choice: "Hindi",
+        isCorrect: false,
+      }
+    ]
+  },
+  {
+    question_aid: 2,
+    question_question:"Q2 Malamig ba ang Icecream?",
+    choice: [
+      {
+        choice: "Pwede",
+        isCorrect: false,
+      },
+      {
+        choice: "Oo",
+        isCorrect: true,
+      },
+      {
+        choice: "Siguro",
+        isCorrect: false,
+      },
+      {
+        choice: "Hindi",
+        isCorrect: false,
+      }
+    ]
+  },{
+    question_aid: 3,
+    question_question:"Q3 May Error ba ako?",
+    choice: [
+      {
+        choice: "Pwede",
+        isCorrect: false,
+      },
+      {
+        choice: "Oo",
+        isCorrect: false,
+      },
+      {
+        choice: "Siguro",
+        isCorrect: true,
+      },
+      {
+        choice: "Hindi",
+        isCorrect: false,
+      }
+    ]
+  },{
+    question_aid: 4,
+    question_question:"Q3 May firworks ba bukas ng umaga?",
+    choice: [
+      {
+        choice: "Pwede",
+        isCorrect: false,
+      },
+      {
+        choice: "Oo",
+        isCorrect: false,
+      },
+      {
+        choice: "Siguro",
+        isCorrect: false,
+      },
+      {
+        choice: "Hindi",
+        isCorrect: true,
+      }
+    ]
+  },
+  {
+    question_aid: 5,
+    question_question:"Q5 Kulay red ba ang pula ng itlog?",
+    choice: [
+      {
+        choice: "Pwede",
+        isCorrect: false,
+      },
+      {
+        choice: "Oo",
+        isCorrect: false,
+      },
+      {
+        choice: "Siguro",
+        isCorrect: false,
+      },
+      {
+        choice: "Hindi",
+        isCorrect: true,
+      }
+    ]
+  }
+
+]
